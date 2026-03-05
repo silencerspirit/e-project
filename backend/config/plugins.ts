02643b148abc3d2f5e1bb570fe97f4b8c6044f82
Портной Владислав <silencerspirit@gmail.com>
2026-03-05 09:35:54 +0300

    chore: Обновлены зависимости, добавлен почтовый провайдер

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,10 +1,29 @@
-export default () => ({
+export default ({ env }: { env: EnvFn }) => ({
   'webp-converter': {
     enabled: true,
     config: {
       mimeTypes: ['image/png', 'image/jpeg', 'image/jpg'],
       options: {
         quality: 80,
+      },
+    },
+  },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'mail.hosting.reg.ru'),
+        port: env.int('SMTP_PORT', 465),
+        secure: env.bool('SMTP_SECURE', true),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        connectionTimeout: env.int('SMTP_CONNECTION_TIMEOUT', 10_000),
+      },
+      settings: {
+        defaultFrom: env('SMTP_DEFAULT_FROM', env('SMTP_USERNAME')),
+        defaultReplyTo: env('SMTP_DEFAULT_REPLY_TO', env('SMTP_USERNAME')),
       },
     },
   },
