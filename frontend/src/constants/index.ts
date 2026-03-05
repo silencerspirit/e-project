9d7970f73e1f91bef7d0b9612ac96b276a0f5d81
Портной Владислав <silencerspirit@gmail.com>
2026-03-05 16:13:07 +0300

    feat: Добавлены страницы ошибок и заглушки

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -3,3 +3,5 @@ export const BASE_CACHE_TIME_MS = 60_000; //1 минута
 export const PRICE_SUFFIX = '₽';
 
 export const PRICE_PER_M2_SUFFIX = `${PRICE_SUFFIX}/м²`;
+
+export const AVAILABLE_PAGES = [{ label: 'Недвижимость', href: '/properties/' }] as const;
