cb86483fe711121ee160c08bd257d16a5aadd1fa
Портной Владислав <silencerspirit@gmail.com>
2026-04-10 12:01:48 +0300

    feat: Добавлена ссылка на сайт в админке

--- backend/src/admin/app.tsx
+++ backend/src/admin/app.tsx
@@ -1,10 +1,25 @@
+import { Globe } from '@strapi/icons';
 import type { StrapiApp } from '@strapi/strapi/admin';
 
 export default {
   config: {
     locales: ['ru'],
   },
-  bootstrap(app: StrapiApp) {
-    console.log(app);
+  register(app: StrapiApp) {
+    app.addMenuLink({
+      to: 'https://alfa-realting.ru/',
+      target: '_blank',
+      icon: Globe,
+      intlLabel: {
+        id: 'app.main-site',
+        defaultMessage: 'Сайт',
+      },
+      position: 9,
+      permissions: [],
+      Component: async () => {
+        return { default: () => null };
+      },
+    });
   },
+  bootstrap() {},
 };
